487037c9330ce9d36dc45eeae13ca32907eb121d
Troy Thomas <>
2024-10-29 19:19:09 -0500

    Created activities and layouts for different pages

---
+++ app/src/main/java/comp4350/recipe_shop_app_version/Activity/SettingsActivity.java
@@ -0,0 +1,87 @@
+package comp4350.recipe_shop_app_version.Activity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import comp4350.recipe_shop_app_version.R;
+
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private BottomNavigationView navBar;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_about);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.about_layout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        navBar = findViewById(R.id.bottomNavigationView);
+        navBar.setSelectedItemId(R.id.about_us);
+
+        setListeners();
+    }//onCreate
+
+    public void setListeners(){
+        navBar.setOnNavigationItemSelectedListener(item -> {
+            boolean success = false;
+            if(item.getItemId() == R.id.search){
+                goToSearch();
+                success = true;
+            }
+            else if(item.getItemId() == R.id.favorites){
+                goToFavorites();
+                success = true;
+            }
+            else if(item.getItemId() == R.id.grocery){
+                goToGrocery();
+                success = true;
+            }
+            else if(item.getItemId() == R.id.about_us){
+                goToAbout();
+                success = true;
+            }
+            return success;
+        });
+    }//setListeners
+
+    private void goToSearch(){
+        Intent finishIntent = new Intent(getApplicationContext(), SearchActivity.class);
+        startActivity(finishIntent);
+    }//goToSearch
+
+    private void goToFavorites(){
+        Intent finishIntent = new Intent(getApplicationContext(), FavoritesActivity.class);
+        startActivity(finishIntent);
+    }//goToFavorites
+
+    private void goToGrocery(){
+        Intent finishIntent = new Intent(getApplicationContext(), GroceryActivity.class);
+        startActivity(finishIntent);
+    }//goToGrocery
+
+    private void goToSettings(){
+        Intent finishIntent = new Intent(getApplicationContext(), SettingsActivity.class);
+        startActivity(finishIntent);
+    }//goToSettings
+
+    private void goToAbout(){
+        Intent finishIntent = new Intent(getApplicationContext(), SettingsActivity.class);
+        startActivity(finishIntent);
+    }//goToAbout
+}
