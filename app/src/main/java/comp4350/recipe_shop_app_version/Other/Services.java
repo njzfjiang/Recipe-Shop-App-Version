89e19024e84183ac7cdae53fcfb3db7ae4df4122
Troy Thomas <>
2024-11-03 20:45:56 -0600

    removed default ip and username (used while testing)

--- app/src/main/java/comp4350/recipe_shop_app_version/Other/Services.java
+++ app/src/main/java/comp4350/recipe_shop_app_version/Other/Services.java
@@ -5,8 +5,8 @@ import android.graphics.Bitmap;
 import org.json.JSONObject;
 
 public class Services {
-    public static String ip = "10.0.0.249"; //temp
-    public static String username = "T5"; //temp
+    public static String ip = ""; //temp
+    public static String username = ""; //temp
     public static String password = "";
     public static String confirm = "";
     public static JSONObject recipe = null;
